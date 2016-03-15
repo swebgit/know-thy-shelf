c0eabfd2eecd1d54cebe070def517b1878f4311f
Sam Weber <spyder255@gmail.com>
2016-03-14 21:19:04 -0500

    Modified return structure of the LogOn action to make it easier to parse

---
+++ KTSWebApi/KTS.Web.Api/Controllers/BrowserController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Reflection;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace KTS.Web.Api.Controllers
+{
+    public class BrowserController : ApiController
+    {
+        [HttpGet]
+        public HttpResponseMessage Index()
+        {
+            var response = new HttpResponseMessage();
+            response.Content = new StringContent($"<html><body>KTS API v{Assembly.GetExecutingAssembly().GetName().Version.ToString()}</body></html>");
+            response.Content.Headers.ContentType = new MediaTypeHeaderValue("text/html");
+            return response;
+        }
+    }
+}
